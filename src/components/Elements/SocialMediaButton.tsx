9cf0113844a65475cbe4807580ec21a21558ca9b
Dima <dima.horbal.dev@gmail.com>
2024-05-12 02:11:09 -0600

    save

--- src/components/Elements/SocialMediaButton.tsx
+++ src/components/Elements/SocialMediaButton.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 type SocialMediaButtonProps = {
   href: string;
-  iconClass: string;
+  icon: IconProp;
   bgColor: string;
   hoverBgColor: string;
   children: ReactNode;
@@ -11,7 +13,7 @@ type SocialMediaButtonProps = {
 
 const SocialMediaButton = ({
   href,
-  iconClass,
+  icon,
   bgColor,
   hoverBgColor,
   children,
@@ -25,7 +27,7 @@ const SocialMediaButton = ({
         rel="noopener noreferrer"
         className={`py-2 px-3 rounded-full text-white bg-${bgColor} hover:bg-${hoverBgColor} transition duration-30`}
       >
-        <i className={`${iconClass}`}></i>
+        <FontAwesomeIcon icon={icon} />
       </a>
     );
   }
@@ -37,7 +39,7 @@ const SocialMediaButton = ({
       rel="noopener noreferrer"
       className={`inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-${bgColor} hover:bg-${hoverBgColor} transition duration-300`}
     >
-      <i className={`${iconClass} mr-2`}></i>
+      <FontAwesomeIcon icon={icon} />
       {children}
     </a>
   );
